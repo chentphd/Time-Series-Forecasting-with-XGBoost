cebb067487fd0989a1209ad7dd3ff463d5cce3ab
chentphd <chentphd@gmail.com>
2024-09-11 18:21:43 -0400

    Time Series Analysis of Energy Data

---
+++ Time Series Forecasting with XGBoost.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns  
+
+import xgboost as xgb 
+
+
+df = pd.read_csv(r"C:\Users\tonychen\Documents\Python Files\Time Series Forecasting with XGBoost\Time Series Forecasting with XGBoost.py")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
